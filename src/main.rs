afe9bad49b72b8b99ee5bfe4a8bc0876711d1c7c
jachan <johnachanya9@gmail.com>
2023-03-02 15:52:22 +0100

    requesting open ai server

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 use std::env;
 use reqwest;
-use std::collections::HashMap;
+use serde_json::{Value, Map, Number};
 
 #[tokio::main]
 async fn main() {
     let name: String = String::from("OPENAI_API_KEY");
-    let OPENAI_API_KEY: String = env::var(name).expect("{name} not found");
+    let openai_api_key: String = env::var(name).expect("{name} not found");
     // chaining .await will yield our query result
-
+    
 
     let client = reqwest::Client::new();
-    let mut data = HashMap::new();
+    let mut data = Map::new();
 
-    data.insert("model", "text-davinci-003");
-    data.insert("prompt", "Say this is a test");
-    data.insert("max_tokens", "7");
+    data.insert("model".to_string(),    Value::String("text-davinci-003".to_string()));
+    data.insert("prompt".to_string(), Value::String("can you generate cv for a fresh engineering graduate?".to_string()));
+    data.insert("max_tokens".to_string(), Value::Number(Number::from(1000)));
 
     let response = client
         .post("https://api.openai.com/v1/completions")
-        .header(reqwest::header::AUTHORIZATION, format!("Bearer {OPENAI_API_KEY}"))
+        .header(reqwest::header::AUTHORIZATION, format!("Bearer {openai_api_key}"))
         .header(reqwest::header::CONTENT_TYPE, "application/json")
         .header(reqwest::header::ACCEPT, "application/json")
         .json(&data)
